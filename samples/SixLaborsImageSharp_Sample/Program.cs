11e87acfe06549fa78fc3a30a3c26c386f666f5c
Eugene <atomnicklol@gmail.com>
2021-12-22 09:28:25 -0600

    Update SixLabrosImageSharp_Sample

---
+++ samples/SixLaborsImageSharp_Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using QoiSharp;
+using QoiSharp.Codec;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        // TODO
+    }
+}
