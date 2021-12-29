b1ab802879e51e91e986af6d8f4c65bb9f8c9ccd
alan.mcgovern <alan.mcgovern@gmail.com>
2021-12-29 18:43:24 +0000

    Improve performance without breaking API
    The original PR was pushed a bit too late and
    wasn't reviewed before 1.0 was released. As such,
    the changes can't be included now without breaking
    API.
    
    Redo some of the changes to gain most of the benefits
    without the API breakage.
    
    1. Nothing in the library uses the public mutable
    state defined in QoiCodec.These properties/fields
    have been replaced with immutable/readonly properties
    instead.
    
    2. QoiEncoder has been optimised a bit to leverage
    the performance gains from BinaryPrimitives and Span<T>.
    Also, a Span<T> based overload was added so that images
    could be encoded into a reusable buffer.
    
    3. QoiDecoder has a new overload which receives a
    Span<T>, allowing images to be decoded from anything
    which can create a Span<byte>, and also allows for
    multiple images to be decoded from the same underlying
    array.

--- src/QoiSharp/Codec/QoiCodec.cs
+++ src/QoiSharp/Codec/QoiCodec.cs
@@ -1,4 +1,6 @@
-﻿namespace QoiSharp.Codec;
+﻿using System.Buffers.Binary;
+
+namespace QoiSharp.Codec;
 
 /// <summary>
 /// QOI Codec.
@@ -13,6 +15,9 @@ public static class QoiCodec
     public const byte Rgba = 0xFF;
     public const byte Mask2 = 0xC0;
 
+    // FIXME: 'MaxPixels' should be 'static readonly' (not const). At the moment
+    // it's mutable so users can reset this to arbitrary values.
+
     /// <summary>
     /// 2GB is the max file size that this implementation can safely handle. We guard
     /// against anything larger than that, assuming the worst case with 5 bytes per 
@@ -20,20 +25,29 @@ public static class QoiCodec
     /// enough for anybody.
     /// </summary>
     public static int MaxPixels = 400_000_000;
-    
+
+    internal static readonly int MaxPixelsReadOnly= 400_000_000;
+
     public const int HashTableSize = 64; 
     
     public const byte HeaderSize = 14;
     public const string MagicString = "qoif";
-    
-    public static readonly int Magic = CalculateMagic(MagicString.AsSpan());
-    public static readonly byte[] Padding = {0, 0, 0, 0, 0, 0, 0, 1};
 
+    public static readonly int Magic = 'q' << 24 | 'o' << 16 | 'i' << 8 | 'f';
+
+    // FIXME: This public API should be removed. This is an implementation detail.
+    [Obsolete("This field should not be used. It will be removed in v2.0")]
+    public static readonly byte[] Padding = new byte[] {0, 0, 0, 0, 0, 0, 0, 1};
+    internal static readonly ReadOnlyMemory<byte> ReadOnlyPadding = new byte[] { 0, 0, 0, 0, 0, 0, 0, 1 };
+
+    // FIXME: This public API should be removed. This is an implementation detail.
+    [Obsolete("This method should not be used. It will be removed in v2.0")]
     public static int CalculateHashTableIndex(int r, int g, int b, int a) =>
         ((r & 0xFF) * 3 + (g & 0xFF) * 5 + (b & 0xFF) * 7 + (a & 0xFF) * 11) % HashTableSize * 4;
 
-    public static bool IsValidMagic(byte[] magic) => CalculateMagic(magic) == Magic;
-    
-    private static int CalculateMagic(ReadOnlySpan<char> chars) => chars[0] << 24 | chars[1] << 16 | chars[2] << 8 | chars[3];
-    private static int CalculateMagic(ReadOnlySpan<byte> data) => data[0] << 24 | data[1] << 16 | data[2] << 8 | data[3];
+    public static bool IsValidMagic(byte[] magic)
+        => IsValidMagic(new ReadOnlySpan<byte>(magic, 0, 4));
+
+    internal static bool IsValidMagic(ReadOnlySpan<byte> magic)
+        => BinaryPrimitives.ReadInt32BigEndian (magic) == Magic;
 }
--- src/QoiSharp/QoiDecoder.cs
+++ src/QoiSharp/QoiDecoder.cs
@@ -1,12 +1,14 @@
 ﻿using QoiSharp.Codec;
 using QoiSharp.Exceptions;
 
+using System.Runtime.InteropServices;
+
 namespace QoiSharp;
 
 /// <summary>
 /// QOI decoder.
 /// </summary>
-public static class QoiDecoder 
+public static class QoiDecoder
 {
     /// <summary>
     /// Decodes QOI data into raw pixel data.
@@ -16,109 +18,104 @@ public static class QoiDecoder
     /// <exception cref="QoiDecodingException">Thrown when data is invalid.</exception>
     public static QoiImage Decode(byte[] data)
     {
-        if (data.Length < QoiCodec.HeaderSize + QoiCodec.Padding.Length)
+        if (data.Length < QoiCodec.HeaderSize + QoiCodec.ReadOnlyPadding.Length)
         {
             throw new QoiDecodingException("File too short");
         }
-        
-        if (!QoiCodec.IsValidMagic(data[..4]))
+
+        if (!QoiCodec.IsValidMagic(data.AsSpan(0, 4)))
         {
             throw new QoiDecodingException("Invalid file magic"); // TODO: add magic value
         }
 
         int width = data[4] << 24 | data[5] << 16 | data[6] << 8 | data[7];
         int height = data[8] << 24 | data[9] << 16 | data[10] << 8 | data[11];
-        byte channels = data[12]; 
+        byte channels = data[12];
         var colorSpace = (ColorSpace)data[13];
 
         if (width == 0)
         {
             throw new QoiDecodingException($"Invalid width: {width}");
         }
-        if (height == 0 || height >= QoiCodec.MaxPixels / width)
+        if (height == 0 || height >= QoiCodec.MaxPixelsReadOnly / width)
         {
-            throw new QoiDecodingException($"Invalid height: {height}. Maximum for this image is {QoiCodec.MaxPixels / width - 1}");
+            throw new QoiDecodingException($"Invalid height: {height}. Maximum for this image is {QoiCodec.MaxPixelsReadOnly / width - 1}");
         }
         if (channels is not 3 and not 4)
         {
             throw new QoiDecodingException($"Invalid number of channels: {channels}");
         }
-        
-        byte[] index = new byte[QoiCodec.HashTableSize * 4];
-        if (channels == 3) // TODO: delete
-        {
-            for (int indexPos = 3; indexPos < index.Length; indexPos += 4)
-            {
-                index[indexPos] = 255;
-            }
-        }
+
+        Span<uint> index = stackalloc uint[QoiCodec.HashTableSize];
+        index.Fill(255);
 
         byte[] pixels = new byte[width * height * channels];
-        
-        byte r = 0;
-        byte g = 0;
-        byte b = 0;
-        byte a = 255;
-        
-        int run = 0;
+
+        byte r = 0, g = 0, b = 0, a = 255;
+
         int p = QoiCodec.HeaderSize;
 
         for (int pxPos = 0; pxPos < pixels.Length; pxPos += channels)
         {
-            if (run > 0)
-            {
-                run--;
-            }
-            else
-            {
-                byte b1 = data[p++];
+            byte b1 = data[p++];
 
-                if (b1 == QoiCodec.Rgb)
-                {
-                    r = data[p++];
-                    g = data[p++];
-                    b = data[p++];
-                }
-                else if (b1 == QoiCodec.Rgba)
-                {
-                    r = data[p++];
-                    g = data[p++];
-                    b = data[p++];
-                    a = data[p++];
-                }
-                else if ((b1 & QoiCodec.Mask2) == QoiCodec.Index)
-                {
-                    int indexPos = (b1 & ~QoiCodec.Mask2) * 4;
-                    r = index[indexPos];
-                    g = index[indexPos + 1];
-                    b = index[indexPos + 2];
-                    a = index[indexPos + 3];
-                }
-                else if ((b1 & QoiCodec.Mask2) == QoiCodec.Diff)
-                {
-                    r += (byte)(((b1 >> 4) & 0x03) - 2);
-                    g += (byte)(((b1 >> 2) & 0x03) - 2);
-                    b += (byte)((b1 & 0x03) - 2);
-                }
-                else if ((b1 & QoiCodec.Mask2) == QoiCodec.Luma) 
-                {
-                    int b2 = data[p++];
-                    int vg = (b1 & 0x3F) - 32;
-                    r += (byte)(vg - 8 + ((b2 >> 4) & 0x0F));
-                    g += (byte)vg;
-                    b += (byte)(vg - 8 + (b2 & 0x0F));
-                }
-                else if ((b1 & QoiCodec.Mask2) == QoiCodec.Run) 
-                {
-                    run = b1 & 0x3F;
-                }
-                
-                int indexPos2 = QoiCodec.CalculateHashTableIndex(r, g, b, a);
-                index[indexPos2] = r;
-                index[indexPos2 + 1] = g;
-                index[indexPos2 + 2] = b;
-                index[indexPos2 + 3] = a;
+            if (b1 == QoiCodec.Rgb)
+            {
+                r = data[p];
+                g = data[p + 1];
+                b = data[p + 2];
+                p += 3;
             }
+            else if (b1 == QoiCodec.Rgba)
+            {
+                r = data[p];
+                g = data[p + 1];
+                b = data[p + 2];
+                a = data[p + 3];
+                p += 4;
+            }
+            else if ((b1 & QoiCodec.Mask2) == QoiCodec.Index)
+            {
+                int indexPos = (b1 & ~QoiCodec.Mask2);
+                var value = index[indexPos];
+                r = (byte)(value >> 24);
+                g = (byte)(value >> 16);
+                b = (byte)(value >> 8);
+                a = (byte)(value >> 0);
+            }
+            else if ((b1 & QoiCodec.Mask2) == QoiCodec.Diff)
+            {
+                r += (byte)(((b1 >> 4) & 0x03) - 2);
+                g += (byte)(((b1 >> 2) & 0x03) - 2);
+                b += (byte)((b1 & 0x03) - 2);
+            }
+            else if ((b1 & QoiCodec.Mask2) == QoiCodec.Luma)
+            {
+                int b2 = data[p++];
+                int vg = (b1 & 0x3F) - 32;
+                r += (byte)(vg - 8 + ((b2 >> 4) & 0x0F));
+                g += (byte)vg;
+                b += (byte)(vg - 8 + (b2 & 0x0F));
+            }
+            else if ((b1 & QoiCodec.Mask2) == QoiCodec.Run && b1 != QoiCodec.Run) // Only execute this block if there's at least 1 additional run.
+            {
+                int run = b1 & 0x3F;
+                int end = pxPos + run * channels;
+                while (pxPos < end)
+                {
+                    pixels[pxPos] = r;
+                    pixels[pxPos + 1] = g;
+                    pixels[pxPos + 2] = b;
+                    if (channels == 4)
+                    {
+                        pixels[pxPos + 3] = a;
+                    }
+                    pxPos += channels;
+                }
+            }
+
+            int indexPos2 = (r * 3 + g * 5 + b * 7 + a * 11) % QoiCodec.HashTableSize;
+            index[indexPos2] = ((uint)r << 24) | ((uint)g << 16) | ((uint)b << 8) | (uint)a;
 
             pixels[pxPos] = r;
             pixels[pxPos + 1] = g;
@@ -128,15 +125,9 @@ public static class QoiDecoder
                 pixels[pxPos + 3] = a;
             }
         }
-        
-        int pixelsEnd = data.Length - QoiCodec.Padding.Length;
-        for (int padIdx = 0; padIdx < QoiCodec.Padding.Length; padIdx++) 
-        {
-            if (data[pixelsEnd + padIdx] != QoiCodec.Padding[padIdx]) 
-            {
-                throw new InvalidOperationException("Invalid padding");
-            }
-        }
+
+        if (!QoiCodec.ReadOnlyPadding.Span.SequenceEqual(data.AsSpan(p, QoiCodec.ReadOnlyPadding.Length)))
+            throw new InvalidOperationException("Invalid padding");
 
         return new QoiImage(pixels, width, height, (Channels)channels, colorSpace);
     }
